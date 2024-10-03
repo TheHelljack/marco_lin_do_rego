7ca9d94733814820bdbc9d1f1cb19ac038e6da62
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-10-03 08:13:30 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,16 @@
+criaCartao(
+    'Física',
+    'Qual a formula do calor específico',
+    'ΔQ = m·c·ΔT'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os três canais do intestino grosso',
+    'Ceco; Cólon Ascendente; Cólon Transversal;'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a fórmula para descobrir número de arestas,.,'
+)
