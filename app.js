f3509731ca6567a12b8a52c53b43131320369174
TheHelljack <132112604+TheHelljack@users.noreply.github.com>
2024-10-03 08:24:13 -0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function criaCartao(categoria, pergunta, resposta){
     cartao.className = 'cartao';
     cartao.innerHTML = `
     <div class = "cartao-conteudo">
-    <h1>${categoria}</h1>
+    <h3>${categoria}</h3>
     <div class = "cartao-pergunta">
         <p>${pergunta}</p>
     </div>
